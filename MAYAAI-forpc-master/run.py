21c6e4e30117b45fd24e537159f34a811f6d9e96
RitikSaini87 <ritiksainibhai5@gmail.com>
2025-10-31 13:57:31 +0530

    Initial upload of MAYAAI-forpc-master project

---
+++ MAYAAI-forpc-master/run.py
@@ -0,0 +1,29 @@
+import multiprocessing
+
+def startMaya():
+    print("Process 1 Starting...")
+    from main import start
+    start()
+
+def listenHotword():
+    print("Process 2 Starting...")
+    from backend.feature import hotword
+    hotword()
+
+if __name__ == "__main__":
+    multiprocessing.set_start_method('spawn', force=True)
+
+    process1 = multiprocessing.Process(target=startMaya)
+    process2 = multiprocessing.Process(target=listenHotword)
+
+    process1.start()
+    process2.start()
+
+    process1.join()
+
+    if process2.is_alive():
+        process2.terminate()
+        print("Process 2 terminated.")
+        process2.join()
+
+    print("System is terminated.")
